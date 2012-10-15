f766187d0b2faa4e303ec066dd26e20fb772eb34
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 02:38:26 +0000

    Fixed header files copyright an license info

--- src/include/tcpip/httpd.h
+++ src/include/tcpip/httpd.h
@@ -1,14 +1,27 @@
-/*
+/* 
+ * Copyright(c) 2004-2012 BORESTE (www.boreste.com). All Rights Reserved.
  *
- * File:	httpd.h
- * Module:
- * Project:	
- * Author:	Robinson Mittmann (bob@boreste.com, bobmittmann@gmail.com)
- * Target:
- * Comment: 
- * Copyright(c) 2004-2009 BORESTE (www.boreste.com). All Rights Reserved.
+ * This file is part of the libtcpip.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file tcpip/httpd.h
+ * @brief 
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __HTTPD_H__
 #define __HTTPD_H__
--- src/include/tcpip/tnshell.h
+++ src/include/tcpip/tnshell.h
@@ -1,14 +1,27 @@
-/* $Id: tnshell.h,v 2.2 2006/04/09 15:03:31 bob Exp $ 
+/* 
+ * Copyright(c) 2004-2012 BORESTE (www.boreste.com). All Rights Reserved.
  *
- * File:	tcpip/tnshell.h
- * Module:
- * Project:
- * Author:	Robinson Mittmann (bob@cnxtech.com, bob@methafora.com.br)
- * Target:
- * Comment: Basic Telnet Shell
- * Copyright(c) 2006 BORESTE (www.boreste.com). All Rights Reserved.
+ * This file is part of the libtcpip.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file tcpip/tnshell.h
+ * @brief 
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __TCPIP_TNSHELL_H__
 #define __TCPIP_TNSHELL_H__
