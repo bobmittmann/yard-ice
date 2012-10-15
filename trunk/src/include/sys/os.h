63579f904245b1228bfe0cd42b4fda852676a08a
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 16:22:38 +0000

    Fixed license info

--- trunk/src/include/sys/os.h
+++ trunk/src/include/sys/os.h
@@ -1,13 +1,27 @@
 /* 
- * File:	sys/os.h
- * Module:	
- * Project:	
- * Author:	Robinson Mittmann (bob@boreste.com, bob@methafora.com.br)
- * Target:	
- * Comment:
- * Copyright(c) 2008 BORESTE (www.boreste.com). All Rights Reserved.
+ * Copyright(C) 2012 Robinson Mittmann. All Rights Reserved.
+ * 
+ * This file is part of the YARD-ICE.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file sys/os.h
+ * @brief YARD-ICE 
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __SYS_OS_H__
 #define __SYS_OS_H__
