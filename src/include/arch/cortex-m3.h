5c8c945c9fdcb5032b16f045189d636b0f8036f5
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 16:08:49 +0000

    Fixed copyright and license info

--- src/include/arch/cortex-m3.h
+++ src/include/arch/cortex-m3.h
@@ -1,25 +1,27 @@
-/*
- * File:	arch/cortex-m3.h
- * Author:   Robinson Mittmann (bobmittmann@gmail.com)
- * Target:	cortex-m3
- * Comment:	Cortex M3 macros and definitions 
- * Copyright(C) 2011 Bob Mittmann. All Rights Reserved.
+/* 
+ * Copyright(C) 2012 Robinson Mittmann. All Rights Reserved.
+ * 
+ * This file is part of the libcm3.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
 
+/** 
+ * @file cortex-m3.h
+ * @brief
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __CORTEX_M3_H__
 #define __CORTEX_M3_H__
