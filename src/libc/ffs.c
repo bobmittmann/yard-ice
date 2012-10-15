ada9ffab1d0c90edd6be65e74afe5a7300738010
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 16:30:39 +0000

    Fixed license info

--- src/libc/ffs.c
+++ src/libc/ffs.c
@@ -1,21 +1,34 @@
-/*
+/* 
+ * Copyright(c) 2004-2012 BORESTE (www.boreste.com). All Rights Reserved.
  *
- * File:    ffs.c
- * Module:
- * Project:
- * Author:	Robinson Mittmann (bob@boreste.com, bobmittmann@gmail.com)
- * Target:
- * Comment: 
- * Copyright(c) 2008 BORESTE (wwww.boreste.com). All Rights Reserved.
+ * This file is part of the YARD-ICE.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file ffs.c
+ * @brief YARD-ICE libc
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #include <string.h>
 
-/* table to convert debruijn index to standard index */
+#define DEBRUIJN32 0x077cb531UL
 
-const char __attribute__((section(".uthreads_data"))) ffs_debruijn_index[32] = 
-{
+/* table to convert debruijn index to standard index */
+const char ffs_debruijn_index[32] = {
 	1, 2, 29, 3, 30, 15, 25, 4, 31, 23, 21, 16, 26, 18, 5, 9, 
 	32, 28, 14, 24, 22, 20, 17, 8, 27, 13, 19, 7, 12, 6, 11, 10
 } ;
