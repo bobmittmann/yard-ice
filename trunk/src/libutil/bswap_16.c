467ab210806c075f8b3769a7a665422dd378e77b
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 18:25:41 +0000

    Fixed license info

--- trunk/src/libutil/bswap_16.c
+++ trunk/src/libutil/bswap_16.c
@@ -1,14 +1,27 @@
-/* $Id: ntohl.c,v 2.0 2006/03/28 15:10:27 bob Exp $ 
+/* 
+ * Copyright(C) 2012 Robinson Mittmann. All Rights Reserved.
+ * 
+ * This file is part of the YARD-ICE.
  *
- * File:	ntohl.c
- * Module:
- * Project:
- * Author:	Robinson Mittmann (bob@cnxtech.com)
- * Target:
- * Comment:	
- * Copyright(c) 2004 CNX Technologies. All Rights Reserved.
- *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file bswap_16.c
+ * @brief YARD-ICE 
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #include <stdint.h>
 
--- trunk/src/libutil/ilog2.c
+++ trunk/src/libutil/ilog2.c
@@ -1,14 +1,27 @@
-/* $Id: ilog2.S,v 1.15 2008/06/08 01:18:27 bob Exp $ 
+/* 
+ * Copyright(C) 2012 Robinson Mittmann. All Rights Reserved.
+ * 
+ * This file is part of the YARD-ICE.
  *
- * File:    ilog2.c
- * Module:
- * Project:
- * Author:	Robinson Mittmann (bob@boreste.com, bobmittmann@gmail.com)
- * Target:
- * Comment: 
- * Copyright(c) 2009 BORESTE (wwww.boreste.com). All Rights Reserved.
- *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file ilog2.c
+ * @brief YARD-ICE 
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #include <string.h>
 #include <stdio.h>
