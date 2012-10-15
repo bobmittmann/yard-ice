cb75e058023bfca112a181636a02e0956be5b28c
bobmittmann <bobmittmann@b8042bbd-3969-42c3-397a-39c25b402966>
2012-10-15 17:18:02 +0000

    Fixed license info

--- trunk/src/include/netinet/tcp.h
+++ trunk/src/include/netinet/tcp.h
@@ -1,14 +1,27 @@
-/* $Id: tcp.h,v 2.1 2008/05/23 03:31:03 bob Exp $ 
+/* 
+ * Copyright(c) 2004-2012 BORESTE (www.boreste.com). All Rights Reserved.
  *
- * File:	netinet/tcp.h
- * Module:	
- * Project:	AT91X40DK	
- * Author:	Robinson Mittmann (bob@cnxtech.com)
- * Target:	arm7tdmi
- * Comment:	generic ipv4 routing scheme	
- * Copyright(c) 2003,2004 CNX Technologies. All Rights Reserved.
+ * This file is part of the YARD-ICE.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file netinet/tcp.h
+ * @brief YARD-ICE libc
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __NETINET_TCP_H__
 #define __NETINET_TCP_H__
--- trunk/src/include/netinet/udp.h
+++ trunk/src/include/netinet/udp.h
@@ -1,14 +1,27 @@
-/* $Id: udp.h,v 2.0 2006/03/28 15:10:05 bob Exp $ 
+/* 
+ * Copyright(c) 2004-2012 BORESTE (www.boreste.com). All Rights Reserved.
  *
- * File:	netinet/udp.h
- * Module:	
- * Project:		
- * Author:	Robinson Mittmann (bob@cnxtech.com)
- * Target:	
- * Comment:	
- * Copyright(c) 2003,2004 CNX Technologies. All Rights Reserved.
+ * This file is part of the YARD-ICE.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You can receive a copy of the GNU Lesser General Public License from 
+ * http://www.gnu.org/
  */
+
+/** 
+ * @file netinet/udp.h
+ * @brief YARD-ICE libc
+ * @author Robinson Mittmann <bobmittmann@gmail.com>
+ */ 
 
 #ifndef __NETINET_UDP_H__
 #define __NETINET_UDP_H__
