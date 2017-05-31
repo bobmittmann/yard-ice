56e01ee1c50d784cffc8e364e8cd9bead557f18b
Bob Mittmann <bobmittmann@gmail.com>
2017-05-31 11:17:58 -0400

    Updated dbg.sh and prog.sh to select last version binary. Also added eclipse project for bootloader.

--- src/app/dbg.sh
+++ src/app/dbg.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
-BUILD_NAME=yard-ice-0.32
-JTAGTOOL_ADDR=192.168.10.51
-TOOLS_DIR=../../thinkos/tools
-THINKAPP_ELF=debug/${BUILD_NAME}.elf
+TOOLS_DIR=../../tools
+PYTHON=python
+if [ -z "$JTAGTOOL_ADDR" ]; then
+	JTAGTOOL_ADDR=192.168.0.51
+fi
+# Collect ".bin" files in the positional parameters
+set -- `ls debug/*.bin`
+# Get the last one
+for PROG_BIN; do true; done
+# The corresponding .elf
+PROG_ELF=${PROG_BIN%%.bin}.elf
 
-${TOOLS_DIR}/tftp_load.py -q -e -r -a 0x08020000 -h ${JTAGTOOL_ADDR} debug/${BUILD_NAME}.bin 
+# Collect ".bin" files in the positional parameters
+set -- `ls ../boot/debug/*.bin`
+# Get the last one
+for THINKOS_BIN; do true; done
+# The corresponding .elf
+THINKOS_ELF=${THINKOS_BIN%%.bin}.elf
 
-if [ $? == 0 ] ; then
-	${TOOLS_DIR}/dcclog -h ${JTAGTOOL_ADDR} ${THINKAPP_ELF}
+${PYTHON} ${TOOLS_DIR}/tftp_load.py -q -i -e -r  -a 0x08020000 \
+	-h ${JTAGTOOL_ADDR} ${PROG_BIN} 
+
+if [ $? = 0 ] ; then
+	# Disable the halt debug mode by clearing C_DEBUGEN on DHCSR
+	${PYTHON} ${TOOLS_DIR}/tftp_cmd.py -h ${JTAGTOOL_ADDR} 'disable debug'
+	if [ $? = 0 ] ; then
+		# Trace
+		${TOOLS_DIR}/dcclog -h ${JTAGTOOL_ADDR} ${THINKOS_ELF} ${PROG_ELF}
+	fi
 fi
 
